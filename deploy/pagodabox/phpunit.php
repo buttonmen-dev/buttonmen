c8598b9582cdb97688f31e45f7c874511be45c3a
Chaos Golubitsky <walrus-github@glassonion.org>
2012-12-05 11:53:43 -0500

    * more cleanup: try putting phpunit into /var/www/deploy-includes

--- deploy/pagodabox/phpunit.php
+++ deploy/pagodabox/phpunit.php
@@ -4,11 +4,11 @@
  * Modelled on /usr/bin/phpunit CLI utility
  */
 
-set_include_path( "/var/www:" . ini_get( "include_path" ) );
+// Include /var/www/deploy-includes for phpunit itself.
+// Include /var/www/src for now, so the individual unit tests can find the base classes
+set_include_path( "/var/www/deploy-includes:/var/www/src" . ini_get( "include_path" ) );
 
 // Tell PHPUnit to run tests relative to the current directory
 $_SERVER['argv'] = array('phpunit.php', '.');
 
 include "phpunit.phar";
-
-PHPUnit_TextUI_Command::main();
