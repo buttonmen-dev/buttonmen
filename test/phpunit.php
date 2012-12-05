ca36320979e1d66b24a5da0197d5c811da6a0bb8
Chaos Golubitsky <walrus-github@glassonion.org>
2012-12-05 11:10:07 -0500

    * first attempt at a phpunit.php file to run all unit tests

---
+++ test/phpunit.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * Run all unit tests
+ * Modelled on /usr/bin/phpunit CLI utility
+ */
+
+define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
+
+set_include_path( "../../../src:" . ini_get( "include_path" ) );
+include "phpunit.phar";
+
+PHPUnit_TextUI_Command::main();
