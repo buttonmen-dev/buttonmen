6b51e9741ed700113cdeefe272d75386419882cc
Chaos Golubitsky <walrus-github@glassonion.org>
2012-12-05 11:12:34 -0500

    * typo

--- test/phpunit.php
+++ test/phpunit.php
@@ -6,7 +6,7 @@
 
 define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
 
-set_include_path( "../../../src:" . ini_get( "include_path" ) );
+set_include_path( "./../../src:" . ini_get( "include_path" ) );
 include "phpunit.phar";
 
 PHPUnit_TextUI_Command::main();
