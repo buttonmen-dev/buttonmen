e26c504338a3a0f5e6506eb8a15648180544b015
Chaos Golubitsky <walrus-github@glassonion.org>
2012-12-05 11:56:12 -0500

    * typo

--- deploy/pagodabox/phpunit.php
+++ deploy/pagodabox/phpunit.php
@@ -6,7 +6,7 @@
 
 // Include /var/www/deploy-includes for phpunit itself.
 // Include /var/www/src for now, so the individual unit tests can find the base classes
-set_include_path( "/var/www/deploy-includes:/var/www/src" . ini_get( "include_path" ) );
+set_include_path( "/var/www/deploy-includes:/var/www/src:" . ini_get( "include_path" ) );
 
 // Tell PHPUnit to run tests relative to the current directory
 $_SERVER['argv'] = array('phpunit.php', '.');
