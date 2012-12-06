92931702ccd7f7e588f4e12d0539f8f5f0d3267e
Julian Lighton <julian@jl8e.org>
2012-12-05 22:55:04 -0500

    Added some more properties

--- src/engine/BMDie.php
+++ src/engine/BMDie.php
@@ -26,15 +26,36 @@ class BMDie
 # Names of the skills that the die has.
 	private $skillList;
 
+# Basic facts about the die
+	public $min;
+	public $max;
+
+	private $scoreValue;
+
     private $mRecipe;
     private $mSides;
     private $mSkills;
 
-    // methods
+	private $doesReroll = true;
+	public $captured = false;
 
-# Run the skill hooks for a given function. $args is an array of the
-#  function arguments. The array itself is passed by value. The
+# This is set when the button may not attack (sleep or focus, for instance)
+# It is set to a string, so the cause may be described. It is cleared at
+# the end of each of your turns.
+	private $inactive = "";
+
+# Set when the button isn't in the game for whatever reason, but could
+#  suddenly join (Warrior Dice)
+	private $unavailable = false;
+
+    // unhooked methods
+
+# Run the skill hooks for a given function. $args is an array of
+#  argumentsfor the function. The array itself is passed by value. The
 #  contents of that array will often be references.
+#
+# By using a static method call, the skill hook methods can use $this
+#  to refer to the die that called them.
 
 	private function run_hooks($func, $args)
 	{
@@ -42,7 +63,6 @@ class BMDie
 
 		foreach ($this->hookLists[$func] as $skill)
 		{
-
 			$skillClass = "BMSkill$skill";
 
 			$skillClass::$func($args);
@@ -54,7 +74,146 @@ class BMDie
 
 	}
 
+# This one may need to be hookable. So might add_skill, depending on
+#  how Chaotic shakes out.
+	public function remove_skill($skill)
+	{
 
+	}
+
+	public function has_skill($skill)
+	{
+
+	}
+
+# This needs to be fixed to work properly within PHP's magic method semantics
+#
+# will need an init_from_db method, too (eventually)
+	public function init($sides, $skills)
+	{
+		$this->min = 1;
+		$this->max = $sides;
+
+		$this->scoreValue = $sides;
+
+		foreach ($skills as $s)
+		{
+			$this->add_skill($s);
+		}
+	}
+
+
+	// hooked methods
+
+# When a die is "woken up" from its container to be used in a
+#  game. Does not roll the die
+
+	public function activate()
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+# Roll the die into a game. Clone self, (possibly into the playDie
+# subclass), roll, return the clone.
+	public function first_roll()
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+
+	public function roll($successfulAttack)
+	{
+		$this->run_hooks(__METHOD__, array($successfulAttack));
+	}
+
+	public function attack_list()
+	{
+		$list = array("Power", "Skill");
+
+		$this->run_hooks(__METHOD__, array(&$list));
+
+		return $list;
+	}
+
+
+	public function attack_values($type)
+	{
+		$attackValueList = array($this->value);
+
+		$this->run_hooks(__METHOD__, array($type, &$attackValueList));
+	}
+
+	public function defense_value($type)
+	{
+		$this->run_hooks(__METHOD__, array($type));
+	}
+
+	public function get_scoreValue()
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+	public function initiative_value()
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+
+	public function validAttack($type, $attackers, $defenders)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+
+	public function validTarget($type, $attackers, $defenders)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+	public function capture($type, $attackers, $victims)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+
+	public function beCaptured($type, $attackers, $victims)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+# Print long description
+	public function describe()
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+	public function split()
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+	public function start_turn($player)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+	public function end_turn($player)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+	public function start_round($player)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+	public function end_round($player)
+	{
+		$this->run_hooks(__METHOD__, array());
+	}
+
+
+	// utility methods
 
     public function __get($property)
     {
