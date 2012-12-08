bd20f23fcf757430af6a60b4a85068272f0fe1b5
James Ong <blackshadowshade@gmail.com>
2012-12-08 14:27:50 +1100

    Added ability to load simple dice with single letter abilities from a recipe, and simple validation of button recipes.

--- src/engine/BMButton.php
+++ src/engine/BMButton.php
@@ -17,20 +17,45 @@ class BMButton {
     // three lists of dice
 
     // methods
-    public function load_from_recipe($recipe) {
+    public function loadFromRecipe($recipe) {
+        $this->validateRecipe($recipe);
         $dieSides = $this->parseRecipeForSides($recipe);
+        $dieSkills = $this->parseRecipeForSkills($recipe);
         unset($this->dieArray);
+
+        // set die sides and skills, one die at a time
         for ($dieIdx = 0; $dieIdx <= (count($dieSides) - 1); $dieIdx++) {
             $tempBMDie = new BMDie;
             $tempBMDie->mSides = $dieSides[$dieIdx];
+            if (!empty($dieSkills[$dieIdx])) {
+                $tempBMDie->mSkills = $dieSkills[$dieIdx];
+            }
             $this->dieArray[] = $tempBMDie;
         }
     }
 
-    private function parseRecipeForSides($recipe) {
-        $dieSizeArray = preg_split('/[^[:digit:]]+/', $recipe);
+    private function validateRecipe($recipe) {
+        $dieArray = preg_split('/[[:space:]]+/', $recipe,
+                               NULL, PREG_SPLIT_NO_EMPTY);
 
+        for ($dieIdx = 0; $dieIdx < count($dieArray); $dieIdx++) {
+            $dieContainsDigit = preg_match('/[[:digit:]]/', $dieArray[$dieIdx]);
+            print($dieContainsDigit);
+            if (1 !== $dieContainsDigit) {
+                throw new InvalidArgumentException("Invalid button recipe.");
+            }
+        }
+    }
+
+    private function parseRecipeForSides($recipe) {
+        $dieSizeArray = preg_split('/[^[:digit:]]+/', $recipe,
+                                   NULL, PREG_SPLIT_NO_EMPTY);
         return $dieSizeArray;
+    }
+
+    private function parseRecipeForSkills($recipe) {
+        $dieSkillArray = preg_split('/[[:digit:][:space:]]+/', $recipe);
+        return $dieSkillArray;
     }
 
     // create dice
