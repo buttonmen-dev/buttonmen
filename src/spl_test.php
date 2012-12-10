e428226b92df028e427ac2f481026e395596c825
Chaos Golubitsky <walrus-github@glassonion.org>
2012-12-10 07:34:31 -0500

    * test spl_enum on pagoda

---
+++ src/spl_test.php
@@ -0,0 +1,27 @@
+<?php
+class Month extends SplEnum {
+    const __default = self::January;
+    
+    const January = 1;
+    const February = 2;
+    const March = 3;
+    const April = 4;
+    const May = 5;
+    const June = 6;
+    const July = 7;
+    const August = 8;
+    const September = 9;
+    const October = 10;
+    const November = 11;
+    const December = 12;
+}
+
+echo new Month(Month::June) . PHP_EOL;
+
+try {
+    new Month(13);
+} catch (UnexpectedValueException $uve) {
+    echo $uve->getMessage() . PHP_EOL;
+}
+?>
+
