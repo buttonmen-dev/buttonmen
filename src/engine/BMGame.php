bfe9615db64ac17f65b915b093db4f7dd996a3b7
James Ong <blackshadowshade@gmail.com>
2012-12-12 01:13:53 +1100

    Added UpdateGameState() skeleton to BMGame. Added unit tests for UpdateGameState() and BMGameState itself.

--- src/engine/BMGame.php
+++ src/engine/BMGame.php
@@ -12,6 +12,7 @@ class BMGame {
     private $playerWithInitiative;  // player who won initiative for this round
     private $buttonArray;           // buttons for all players
     private $activeDieArrayArray;   // active dice for all players
+    private $passStatusArray;       // boolean array whether each player passed
     private $capturedDieArrayArray; // captured dice for all players
     private $roundScoreArray;       // current points score in this round
     private $gameScoreArray;        // number of games W/T/L for all players
@@ -19,6 +20,67 @@ class BMGame {
     private $gameState;             // current game state as a BMGameState enum
 
     // methods
+    public function updateGameState () {
+        switch ($this->gameState) {
+            case BMGameState::startGame:
+                $this->gameState = BMGameState::chooseAuxiliaryDice;
+                break;
+
+            case BMGameState::applyHandicaps:
+                break;
+
+            case BMGameState::chooseAuxiliaryDice:
+                $this->gameState = BMGameState::loadDice;
+                break;
+
+            case BMGameState::loadDice:
+                $this->gameState = BMGameState::specifyDice;
+                break;
+
+            case BMGameState::specifyDice:
+                $this->gameState = BMGameState::determineInitiative;
+                break;
+
+            case BMGameState::determineInitiative:
+                $this->gameState = BMGameState::startGame;
+                break;
+
+            case BMGameState::startRound:
+                $this->gameState = BMGameState::startTurn;
+                break;
+
+            case BMGameState::startTurn:
+                $this->gameState = BMGameState::endTurn;
+                break;
+
+            case BMGameState::endTurn:
+                $nDice = array_map("count", $this->activeDieArrayArray);
+                // check if any player has no dice, or if everyone has passed
+                if ((0 === min($nDice)) ||
+                    !in_array(FALSE, $this->passStatusArray, TRUE)) {
+                    $this->gameState = BMGameState::endRound;
+                } else {
+                    $this->gameState = BMGameState::startTurn;
+                }
+                break;
+
+            case BMGameState::endRound:
+                $this->gameState = BMGameState::loadDice;
+                for ($playerIdx = 0; $playerIdx < count($this->gameScoreArray) ; $playerIdx++) {
+                    if ($this->gameScoreArray[$playerIdx]['W'] >= $this->maxWins) {
+                        $this->gameState = BMGameState::endGame;
+                        break;
+                    }
+                }
+                break;
+
+            case BMGameState::endGame:
+                break;
+
+            default:
+                break;
+        }
+    }
 
     // utility methods
 
@@ -35,15 +97,30 @@ class BMGame {
     public function __set($property, $value)
     {
         switch ($property) {
+            case 'gameScoreArray':
+                if (count($this->playerArray) != count($value)) {
+                    throw new InvalidArgumentException('Invalid number of W/L/T results provided.');
+                }
+                $tempArray = array();
+                for ($playerIdx = 0; $playerIdx < count($value); $playerIdx++) {
+                    // check whether there are three inputs and they are all positive
+                    if ((3 !== count($value[$playerIdx])) ||
+                        min(array_map('min', $value)) < 0) {
+                        throw new InvalidArgumentException('Invalid W/L/T array provided.');
+                    }
+                    $tempArray[$playerIdx] = array('W' => $value[$playerIdx][0],
+                                                   'L' => $value[$playerIdx][1],
+                                                   'D' => $value[$playerIdx][2]);
+                }
+                $this->gameScoreArray = $tempArray;
+                break;
             default:
                 $this->$property = $value;
         }
     }
 }
 
-class BMGameState extends SplEnum {
-    const __default = self::startGame;
-
+class BMGameState {
     // pre-game
     const startGame = 10;
     const applyHandicaps = 11;
