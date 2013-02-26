4c4d57d351ace8a8c4bda9f0a4837b0115f952cc
James Ong <blackshadowshade@gmail.com>
2013-02-27 02:02:35 +1100

    Altered attack validation code to automatically detect the attacker and defender, and their corresponding dice

--- src/api/test_submit.php
+++ src/api/test_submit.php
@@ -4,25 +4,49 @@
     require_once '../engine/BMAttack.php';
 
     $game = loadMockGameData();
+    // load POST string, which should be something like:
+    // playerIdx_1_dieIdx_1 playerIdx_0_dieIdx_2
     $selectedDiceString = $_POST['selectedDice'];
-
-
-    $attackerIdx = 1;
-    $defenderIdx = 0;
-
-    $string = preg_replace("/[^[:digit:]]+/", " ", $selectedDiceString);
+    // parse string into its component values, which should end up with something like
+    // 1 1 0 2
     $stringArray = preg_split("/[^[:digit:]]+/", $selectedDiceString, NULL, PREG_SPLIT_NO_EMPTY);
 
-    $attackers = array($game->activeDieArrayArray[$stringArray[0]][$stringArray[1]]);
-    $defenders = array($game->activeDieArrayArray[$stringArray[2]][$stringArray[3]]);
+    // ensure that enough dice have been selected for an attack
+    // james: only power attacks are currently implemented here
+    $arrayLength = count($stringArray);
+    if ($arrayLength < 2) {
+        return;
+    }
 
+    // determine attacker and defender indices, using the first and last die
+    // selected by the user
+    $attackerIdx = $stringArray[0];
+    $defenderIdx = $stringArray[$arrayLength - 2];
+
+    // divide selected dice up into attackers and defenders
+    $arrayIdx = 0;
+    while ($arrayIdx < $arrayLength) {
+        if ($attackerIdx === $stringArray[$arrayIdx]) {
+            $arrayIdx++;
+            $attackers[] = $game->activeDieArrayArray[$attackerIdx][$stringArray[$arrayIdx]];
+        } elseif ($defenderIdx === $stringArray[$arrayIdx]) {
+            $arrayIdx++;
+            $defenders[] = $game->activeDieArrayArray[$defenderIdx][$stringArray[$arrayIdx]];
+        } else {
+            throw new LogicException('There can only be one attacker and one defender.');
+        }
+
+        $arrayIdx++;
+    }
+
+    // validate attack
     $attack = BMAttackPower::get_instance();
     $success = $attack->validate_attack($game, $attackers, $defenders);
 
-//    echo var_dump($stringArray);
+    // output the result of the attack
     if ($success) {
-      echo 'attack successful';
+      echo 'attack valid';
     } else {
-      echo 'attack failed';
+      echo 'attack invalid';
     }
 ?>
