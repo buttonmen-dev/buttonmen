84a7e5429ba30d27533cdc260a6d73f5ef273229
Chaos Golubitsky <walrus-github@glassonion.org>
2013-03-31 22:05:16 -0400

    * more directory testing

--- src/api/json_test_cache_data.php
+++ src/api/json_test_cache_data.php
@@ -8,7 +8,15 @@ $gameDataJson = json_encode($game->getJsonData());
 header('Content-Type: application/json');
 echo $gameDataJson;
 
-echo readdir('/');
+$dir = '/';
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            echo "filename: $file : filetype: " . filetype($dir . $file) . "\n";
+        }
+        closedir($dh);
+    }
+}
 
 file_put_contents('/bmgame/gamedata.json', $gameDataJson);
 
