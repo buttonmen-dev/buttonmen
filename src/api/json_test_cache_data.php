a39e3c3862bab9093cdac85ff53d3d206fa72839
Chaos Golubitsky <walrus-github@glassonion.org>
2013-03-31 22:20:10 -0400

    * try caching the file

--- src/api/json_test_cache_data.php
+++ src/api/json_test_cache_data.php
@@ -5,19 +5,19 @@ require_once 'loadMockGameData.php';
 $game = loadMockGameData();
 $gameDataJson = json_encode($game->getJsonData());
 
-header('Content-Type: text/plain');
-//echo $gameDataJson;
+header('Content-Type: application/json');
+echo $gameDataJson
 
-$dir = '/var/www/';
-if (is_dir($dir)) {
-    if ($dh = opendir($dir)) {
-        while (($file = readdir($dh)) !== false) {
-            echo "filename: $file : filetype: " . filetype($dir . $file) . "\n";
-        }
-        closedir($dh);
-    }
-}
+//$dir = '/var/www/';
+//if (is_dir($dir)) {
+//    if ($dh = opendir($dir)) {
+//        while (($file = readdir($dh)) !== false) {
+//            echo "filename: $file : filetype: " . filetype($dir . $file) . "\n";
+//        }
+//        closedir($dh);
+//    }
+//}
 
-// file_put_contents('/bmgame/gamedata.json', $gameDataJson);
+file_put_contents('/var/www/bmgame/gamedata.json', $gameDataJson);
 
 ?>
