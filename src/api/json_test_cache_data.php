79a3bb72800e9d361d7784e744ebd5a29eaef84b
Chaos Golubitsky <walrus-github@glassonion.org>
2013-03-31 21:49:46 -0400

    * try a full path for the shared directory

--- src/api/json_test_cache_data.php
+++ src/api/json_test_cache_data.php
@@ -5,7 +5,7 @@ require_once 'loadMockGameData.php';
 $game = loadMockGameData();
 $gameDataJson = json_encode($game->getJsonData());
 
-file_put_contents('shared/bmgame/gamedata.json', $gameDataJson);
+file_put_contents('/var/www/shared/bmgame/gamedata.json', $gameDataJson);
 
 header('Content-Type: application/json');
 echo $gameDataJson;
