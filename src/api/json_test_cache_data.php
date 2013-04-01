d22b0ed3c3845f9f33d8f47be1ed00029930ce99
Chaos Golubitsky <walrus-github@glassonion.org>
2013-03-31 21:46:34 -0400

    * flimsy test of data caching

---
+++ src/api/json_test_cache_data.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'loadMockGameData.php';
+
+$game = loadMockGameData();
+$gameDataJson = json_encode($game->getJsonData());
+
+file_put_contents('shared/bmgame/gamedata.json', $gameDataJson);
+
+header('Content-Type: application/json');
+echo $gameDataJson;
+?>
