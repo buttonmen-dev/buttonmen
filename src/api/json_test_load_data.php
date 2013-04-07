8d674a45a23ee1254cc9fb1121d424fdd626ab62
Chaos Golubitsky <walrus-github@glassonion.org>
2013-04-06 21:15:20 -0400

    * check game status within api test function

--- src/api/json_test_load_data.php
+++ src/api/json_test_load_data.php
@@ -5,8 +5,14 @@ require_once '../engine/BMGame.php';
 
 $gameDataJson = file_get_contents('/var/www/bmgame/gamedata.json');
 $gameData = json_decode($gameDataJson);
-
 header('Content-Type: text/plain');
-var_dump($gameData);
+
+if $gameData["status"] == "ok" {
+  echo "Game status is ok.  Game data:"
+  var_dump($gameData["data"]);
+} else {
+  echo "Game status is NOT ok.  Loaded object data:"
+  var_dump($gameData);
+}
 
 ?>
