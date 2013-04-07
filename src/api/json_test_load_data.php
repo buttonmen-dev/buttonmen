5caeb5dbabb3115aa1d924c03319ea7ea2ed8055
Chaos Golubitsky <walrus-github@glassonion.org>
2013-04-06 21:21:34 -0400

    * typoes

--- src/api/json_test_load_data.php
+++ src/api/json_test_load_data.php
@@ -7,9 +7,9 @@ $gameDataJson = file_get_contents('/var/www/bmgame/gamedata.json');
 $gameData = json_decode($gameDataJson);
 header('Content-Type: text/plain');
 
-if ($gameData["status"] == "ok") {
+if ($gameData->["status"] == "ok") {
   echo "Game status is ok.  Game data:";
-  var_dump($gameData["data"]);
+  var_dump($gameData->["data"]);
 } else {
   echo "Game status is NOT ok.  Loaded object data:";
   var_dump($gameData);
