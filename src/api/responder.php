f57373990c3543ce21c4cd337e7c36ffbd96d6bb
James Ong <blackshadowshade@gmail.com>
2013-04-28 01:27:50 +1000

    james: added logic to create_game to check player names

--- src/api/responder.php
+++ src/api/responder.php
@@ -6,10 +6,24 @@
     header('Content-Type: application/json');
 
     switch ($_POST['type']) {
+        case 'loadPlayerName':
+            echo json_encode(array('status' => 'ok',
+                                   'data' => 'blackshadowshade'));
+            break;
         case 'loadButtonNames':
             $dataArray = array(
                 'buttonNameArray' => $interface->get_all_button_names());
             echo json_encode($dataArray);
+            break;
+        case 'checkPlayerNames':
+            $arePlayerNamesValid = TRUE;
+            foreach ($_POST['playerNameArray'] as $playerName) {
+                if ('' == $interface->get_player_id_from_name($playerName)) {
+                    $arePlayerNamesValid = FALSE;
+                }
+            }
+            echo json_encode(array('status' => 'ok',
+                                   'data' => $arePlayerNamesValid));
             break;
         case 'chooseButtons':
             $buttonNameArray = $_POST['buttonNameArray'];
@@ -17,7 +31,8 @@
                                    'data' => $buttonNameArray));
             break;
         case 'submitSwingValues':
-            echo json_encode(array('status' => 'ok', 'data' => 'hello'));
+            echo json_encode(array('status' => 'ok',
+                                   'data' => 'created game'));
             break;
         default:
             //do nothing
