3c66bb1e550b385431eb6dac49276a52715b42cb
blackshadowshade <blackshadowshade@gmail.com>
2013-05-21 11:00:14 +1000

    explicitly give port number

--- src/database/mysql.inc.php
+++ src/database/mysql.inc.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $conn = new PDO('mysql:host=localhost;dbname=buttonmen', 'karlene', 'Xb4jBUSs');
+        $conn = new PDO('mysql:host=127.0.0.1;port=3306;dbname=buttonmen', 'karlene', 'Xb4jBUSs');
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
