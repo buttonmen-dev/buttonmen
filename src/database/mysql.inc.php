71485a954ab74a76d8ee62f404b7d299bb93d639
blackshadowshade <blackshadowshade@gmail.com>
2013-05-21 14:03:49 +1000

    use tunnel.pagodabox.com to connect

--- src/database/mysql.inc.php
+++ src/database/mysql.inc.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $conn = new PDO('mysql:host=localhost;unix_socket=/tmp/mysql/buttonmen.sock;dbname=buttonmen', 'karlene', 'Xb4jBUSs');
+        $conn = new PDO('mysql:host=tunnel.pagodabox.com;port=3306;dbname=buttonmen', 'karlene', 'Xb4jBUSs');
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
