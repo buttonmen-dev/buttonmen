7ab0d6625b321adb79b58f00034ca36baf3e1788
James Ong <blackshadowshade@gmail.com>
2013-05-22 22:18:17 +1000

    Added comments to MySQL PHP connection script

--- src/database/mysql.inc.php
+++ src/database/mysql.inc.php
@@ -1,11 +1,13 @@
 <?php
     if (isset($_SERVER['DB1_HOST'])) {
+        // pagodabox connection details are stored in environment variables
         $host = $_SERVER['DB1_HOST'];
         $port = $_SERVER['DB1_PORT'];
         $name = $_SERVER['DB1_NAME'];
         $user = $_SERVER['DB1_USER'];
         $pass = $_SERVER['DB1_PASS'];
     } else {
+        // set connection details for local test server
         $host = 'localhost';
         $port = 3306;
         $name = 'buttonmen';
