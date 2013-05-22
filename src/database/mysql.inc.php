1c6ef49ad363b358a3c8291ce2d8a6da1a759d6d
James Ong <blackshadowshade@gmail.com>
2013-05-22 22:15:51 +1000

    Added authentication details for test server using localhost

--- src/database/mysql.inc.php
+++ src/database/mysql.inc.php
@@ -1,10 +1,18 @@
 <?php
-    $host = $_SERVER['DB1_HOST'];
-    $port = $_SERVER['DB1_PORT'];
-    $name = $_SERVER['DB1_NAME'];
-    $user = $_SERVER['DB1_USER'];
-    $pass = $_SERVER['DB1_PASS'];
-    
+    if (isset($_SERVER['DB1_HOST'])) {
+        $host = $_SERVER['DB1_HOST'];
+        $port = $_SERVER['DB1_PORT'];
+        $name = $_SERVER['DB1_NAME'];
+        $user = $_SERVER['DB1_USER'];
+        $pass = $_SERVER['DB1_PASS'];
+    } else {
+        $host = 'localhost';
+        $port = 3306;
+        $name = 'buttonmen';
+        $user = 'root';
+        $pass = 'root';
+    }
+
     try {
         $conn = new PDO("mysql:host=$host;port=$port;dbname=$name", $user, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
