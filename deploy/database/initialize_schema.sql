1bc9d32a48ece8c561a9698444060419ec447f2c
Chaos Golubitsky <walrus-github@glassonion.org>
2013-06-22 17:55:34 -0400

    * auto_increment_increment matters when inserting data, not when initializing tables

--- deploy/database/initialize_schema.sql
+++ deploy/database/initialize_schema.sql
@@ -2,9 +2,6 @@
 
 use buttonmen;
 
-# Pagoda defaults to auto_increment of 4, which isn't what we want
-set auto_increment_increment=1;
-
 # Initialize schemas
 source schema.button.sql;
 source schema.player.sql;
