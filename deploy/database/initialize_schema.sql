3e91fe3d81728695dafe2da645ab22f40f9ba880
Chaos Golubitsky <walrus-github@glassonion.org>
2013-06-22 17:15:50 -0400

    * override pagoda default of auto_increment=4

--- deploy/database/initialize_schema.sql
+++ deploy/database/initialize_schema.sql
@@ -2,6 +2,9 @@
 
 use buttonmen;
 
+# Pagoda defaults to auto_increment of 4, which isn't what we want
+set auto_increment_increment=1;
+
 # Initialize schemas
 source schema.button.sql;
 source schema.player.sql;
