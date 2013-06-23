98a7f63f2d4201b6176168d93d2bb2e1ea3e2e74
Chaos Golubitsky <walrus-github@glassonion.org>
2013-06-23 16:27:45 -0400

    * make some database table names more consistent * try to fix pagodabox "increment by four" issue in database * clarify grunt README

--- deploy/database/data.button.sql
+++ deploy/database/data.button.sql
@@ -1,20 +1,20 @@
-DELETE FROM button_sets;
-INSERT INTO button_sets (name) VALUES
+DELETE FROM buttonset;
+INSERT INTO buttonset (name) VALUES
 ('Soldiers'),
 ('Brom');
 
-DELETE FROM button_definitions;
-INSERT INTO button_definitions (name, recipe, tourn_legal, set_id) VALUES
-('Avis', '4 4 10 12 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Hammer', '6 12 20 20 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Bauer', '8 10 12 20 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Stark', '4 6 8 X X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Clare', '6 8 8 20 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Kith', '6 8 12 12 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Karl', '4 6 6 20 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Iago', '20 20 20 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Niles', '6 10 10 12 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Shore', '4 4 20 20 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Hannah', '8 10 10 10 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Kublai', '4 8 12 20 X', 1, (SELECT id FROM button_sets WHERE name="Soldiers")),
-('Changeling', 'X X X X X', 0, (SELECT id FROM button_sets WHERE name="Soldiers"));
+DELETE FROM button;
+INSERT INTO button (name, recipe, tourn_legal, set_id) VALUES
+('Avis', '4 4 10 12 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Hammer', '6 12 20 20 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Bauer', '8 10 12 20 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Stark', '4 6 8 X X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Clare', '6 8 8 20 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Kith', '6 8 12 12 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Karl', '4 6 6 20 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Iago', '20 20 20 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Niles', '6 10 10 12 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Shore', '4 4 20 20 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Hannah', '8 10 10 10 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Kublai', '4 8 12 20 X', 1, (SELECT id FROM buttonset WHERE name="Soldiers")),
+('Changeling', 'X X X X X', 0, (SELECT id FROM buttonset WHERE name="Soldiers"));
--- deploy/database/views.button.sql
+++ deploy/database/views.button.sql
@@ -3,6 +3,6 @@
 DROP VIEW IF EXISTS button_view;
 CREATE VIEW button_view
 AS SELECT d.name, d.recipe, d.tourn_legal, d.image_path, s.name AS set_name
-FROM button_definitions AS d
-LEFT JOIN button_sets AS s
+FROM button AS d
+LEFT JOIN buttonset AS s
 ON d.set_id = s.id;
