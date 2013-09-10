30e051bc9664123e7cf60f14db056b15bf821a60
James Ong <blackshadowshade@gmail.com>
2013-09-11 00:45:13 +1000

    Added BMSkillPoison framework

---
+++ src/engine/BMSkillPoison.php
@@ -0,0 +1,12 @@
+<?php
+
+class BMSkillPoison extends BMSkill {
+
+    public static $name = "Poison";
+    public static $abbrev = "p";
+
+    public static $hooked_methods = array("value");
+
+}
+
+?>
---
+++ test/src/engine/BMSkillPoisonTest.php
@@ -0,0 +1,34 @@
+<?php
+
+class BMSkillPoisonTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @var BMSkillPoison
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new BMSkillPoison;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     *
+     */
+    public function testDummy() {
+        
+    }
+}
+
+?>
