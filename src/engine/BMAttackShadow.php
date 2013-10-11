b32c0833a97628caed95bd4c227401e9aa5c13b9
James Ong <blackshadowshade@gmail.com>
2013-10-12 03:26:16 +1100

    Fixed code to work with PHP 5.3

--- src/engine/BMAttackShadow.php
+++ src/engine/BMAttackShadow.php
@@ -5,11 +5,30 @@ class BMAttackShadow extends BMAttackPower {
     public $type = 'Shadow';
 
     public function validate_attack($game, array $attackers, array $defenders) {
-        return (1 == count($attackers) &&
-                1 == count($defenders) &&
-                $attackers[0]->max >= $defenders[0]->defense_value($this->type) &&
-                $attackers[0]->attack_values($this->type)[0] <= $defenders[0]->defense_value($this->type) &&
-                $attackers[0]->valid_attack($this->type, $attackers, $defenders) &&
-                $defenders[0]->valid_target($this->type, $attackers, $defenders));
+        if (1 != count($attackers) || 1 != count($defenders)) {
+            return FALSE;
+        }
+
+        $attacker = $attackers[0];
+        $defender = $defenders[0];
+
+        $isDieLargeEnough = $attacker->max >=
+                            $defender->defense_value($this->type);
+
+        $attackValueArray = $attacker->attack_values($this->type);
+        assert(1 == count($attackValueArray));
+        $attackValue = $attackValueArray[0];
+        $defenseValue = $defender->defense_value($this->type);
+        $isValueSmallEnough = $attackValue <= $defenseValue;
+
+        $canAttackerPerformThisAttack =
+            $attacker->valid_attack($this->type, $attackers, $defenders);
+        $isDefenderValidTargetForThisAttack =
+            $defender->valid_target($this->type, $attackers, $defenders);
+
+        return ($isDieLargeEnough &&
+                $isValueSmallEnough &&
+                $canAttackerPerformThisAttack &&
+                $isDefenderValidTargetForThisAttack);
     }
 }
