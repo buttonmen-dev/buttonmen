5e5a8410552bac7163a80e9efea0cc26be3b0154
blackshadowshade <blackshadowshade@gmail.com>
2013-11-20 15:24:05 +1100

    Added logic to write BMGame->nRecentPasses to the database on a save_game(), and read it on a load_game(). Added unit tests.

--- src/engine/BMInterface.php
+++ src/engine/BMInterface.php
@@ -35,12 +35,13 @@ class BMInterface {
         try {
             // create basic game details
             $query = 'INSERT INTO game '.
-                     '(n_players, n_target_wins, creator_id) '.
+                     '(n_players, n_target_wins, n_recent_passes, creator_id) '.
                      'VALUES '.
-                     '(:n_players, :n_target_wins, :creator_id)';
+                     '(:n_players, :n_target_wins, :n_recent_passes, :creator_id)';
             $statement = self::$conn->prepare($query);
-            $statement->execute(array(':n_players'     => count($playerIdArray),
+            $statement->execute(array(':n_players'     => count($playerIdArray),                                      
                                       ':n_target_wins' => $maxWins,
+                                      ':n_recent_passes' => 0,
                                       ':creator_id'    => $playerIdArray[0]));
 
             $statement = self::$conn->prepare('SELECT LAST_INSERT_ID()');
@@ -111,6 +112,7 @@ class BMInterface {
                     $game->gameId    = $gameId;
                     $game->gameState = $row['game_state'];
                     $game->maxWins   = $row['n_target_wins'];
+                    $game->nRecentPasses = $row['n_recent_passes'];
                     $this->timestamp = new DateTime($row['last_action_time']);
                 }
 
@@ -153,10 +155,6 @@ class BMInterface {
                 if ($row['did_win_initiative']) {
                     $game->playerWithInitiativeIdx = $pos;
                 }
-
-                // james: currently, mock nRecentPasses
-                $game->nRecentPasses = 0;
-
             }
 
             // check whether the game exists
@@ -226,10 +224,11 @@ class BMInterface {
 
             $game->activeDieArrayArray = $activeDieArrayArray;
             $game->capturedDieArrayArray = $capturedDieArrayArray;
-
+            
             $game->proceed_to_next_user_action();
 
             $this->message = $this->message."Loaded data for game $gameId.";
+            
             return $game;
         } catch (Exception $e) {
             $this->message = "Game load failed: $e";
@@ -261,7 +260,7 @@ class BMInterface {
                      'SET game_state = :game_state,'.
                      '    round_number = :round_number,'.
             //:n_recent_draws
-            //:n_recent_passes
+                     '    n_recent_passes = :n_recent_passes,'.
                      '    current_player_id = :current_player_id '.
             //:last_winner_id
             //:tournament_id
@@ -271,6 +270,7 @@ class BMInterface {
             $statement = self::$conn->prepare($query);
             $statement->execute(array(':game_state' => $game->gameState,
                                       ':round_number' => $game->roundNumber,
+                                      ':n_recent_passes' => $game->nRecentPasses,
                                       ':current_player_id' => $currentPlayerId,
                                       ':game_id' => $game->gameId));
 
@@ -596,6 +596,5 @@ class BMInterface {
         }
     }
 }
-
 
 ?>
