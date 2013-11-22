f5385b5bb9f8cf87562228d970281f0b2bddfdb0
Chaos Golubitsky <walrus-github@glassonion.org>
2013-11-22 15:50:53 -0500

    * vagrant configuration to work with Amazon AWS * install and customize login accounts for chaos,james on vagrant nodes * have vagrant install puppet locally rather than try to load it   via the filesystem

---
+++ deploy/vagrant/modules/sudo/manifests/init.pp
@@ -0,0 +1,8 @@
+class sudo::buttonmen-devs {
+  file {
+    "/etc/sudoers.d/99-buttonmen-dev":
+      ensure => file,
+      mode => 0440,
+      content => "%admin ALL=(ALL) NOPASSWD:ALL\n";
+  }
+}
