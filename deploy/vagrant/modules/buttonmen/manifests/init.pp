4983254863e7af4bf6de179a782f5ab8af871afe
Chaos Golubitsky <walrus-github@glassonion.org>
2013-12-19 23:36:45 -0500

    * audit wrapper which runs all CLI-compatible audit/unit tests

--- deploy/vagrant/modules/buttonmen/manifests/init.pp
+++ deploy/vagrant/modules/buttonmen/manifests/init.pp
@@ -22,6 +22,11 @@ class buttonmen::server {
       content => template("buttonmen/create_databases.erb"),
       mode => 0555;
 
+    "/usr/local/bin/run_buttonmen_tests":
+      ensure => file,
+      content => template("buttonmen/run_buttonmen_tests.erb"),
+      mode => 0555;
+
     "/usr/local/bin/audit_js_unit_test_coverage":
       ensure => file,
       content => template("buttonmen/audit_js_unit_test_coverage.erb"),
