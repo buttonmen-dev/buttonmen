6e07e445768abb2da2c8c9565a46d4a56bd206e4
blackshadowshade <blackshadowshade@gmail.com>
2014-01-16 22:25:26 +1100

    Added simple unit test to BMSkillMorphingTest

--- test/src/engine/BMSkillMorphingTest.php
+++ test/src/engine/BMSkillMorphingTest.php
@@ -23,39 +23,20 @@ class BMSkillMorphingTest extends PHPUnit_Framework_TestCase {
     {
     }
 
-//    /**
-//     * @covers BMSkillSpeed::attack_list
-//     */
-//    public function testAttack_list()
-//    {
-//        $a = array();
-//        $b = array('attackTypeArray' => &$a);
-//
-//        $this->object->attack_list($b);
-//        // Test adding Speed
-//        $this->assertNotEmpty($a);
-//        $this->assertContains('Speed', $a);
-//        // Only once
-//        $this->assertEquals(1, count($a));
-//
-//        // Test adding Speed to a non-empty array
-//        $a = array('Power' => 'Power', 'Skill' => 'Skill');
-//        $this->object->attack_list($b);
-//        $this->assertNotEmpty($a);
-//        $this->assertEquals(3, count($a));
-//        $this->assertContains('Speed', $a);
-//        // Confirm other contents intact
-//        $this->assertContains('Power', $a);
-//        $this->assertContains('Skill', $a);
-//
-//        // Check adding Speed to an array already containing Speed
-//        $a = array('Speed' => 'Speed', 'Skill' => 'Skill');
-//        $this->object->attack_list($b);
-//        $this->assertNotEmpty($a);
-//        $this->assertContains('Speed', $a);
-//        $this->assertEquals(2, count($a));
-//
-//    }
+    /**
+     * @covers BMSkillMorphing::capture
+     */
+    public function testCapture() {
+        $att = BMDie::create(6);
+        $att->add_skill('Morphing');
+
+        $def = BMDie::create(17);
+
+        BMSkillMorphing::capture(array('type' => 'Power',
+                                       'attackers' => array($att),
+                                       'defenders' => array($def)));
+        $this->assertEquals(17, $att->max);
+    }
 }
 
 ?>
