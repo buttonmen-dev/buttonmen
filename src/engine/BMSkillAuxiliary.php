b09298f58128be01b7c0493cad708912f6a46f1e
James Ong <blackshadowshade@gmail.com>
2014-01-30 00:24:16 +1100

    Added files needed for the auxiliary skill

---
+++ src/engine/BMSkillAuxiliary.php
@@ -0,0 +1,9 @@
+<?php
+
+class BMSkillAuxiliary extends BMSkill {
+    public static $hooked_methods = array("");
+
+//    public static function react_to_initiative() {
+//        return TRUE;
+//    }
+}
---
+++ test/src/engine/BMSkillAuxiliaryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+class BMSkillAuxiliaryTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @var BMSkillAuxiliary
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new BMSkillAuxiliary;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+//    /**
+//     * @covers BMSkillAuxiliary::react_to_initiative
+//     */
+//    public function testReact_to_initiative()
+//    {
+//        $this->assertTrue($this->object->react_to_initiative());
+//    }
+}
+
+?>
