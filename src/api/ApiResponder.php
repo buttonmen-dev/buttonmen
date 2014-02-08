513a1d643df621e39baffd1a1684641f1e25faa3
James Ong <blackshadowshade@gmail.com>
2014-02-09 08:03:00 +1100

    Added responder call to BMInterface->react_to_initiative()

--- src/api/ApiResponder.php
+++ src/api/ApiResponder.php
@@ -137,6 +137,19 @@ class ApiResponder {
         );
     }
 
+    protected function get_interface_response_reactToReserve($interface, $args) {
+        if (!(array_key_exists('dieIdx', $args))) {
+            $args['dieIdx'] = NULL;
+        }
+
+        return $interface->react_to_reserve(
+            $_SESSION['user_id'],
+            $args['game'],
+            $args['action'],
+            $args['dieIdxArray']
+        );
+    }
+
     protected function get_interface_response_reactToInitiative($interface, $args) {
         if (!(array_key_exists('dieIdxArray', $args))) {
             $args['dieIdxArray'] = NULL;
