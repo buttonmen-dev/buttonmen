c59cb304088e2d3d5a9ef21c5542ddbf2e4fcce8
Chaos Golubitsky <walrus-github@glassonion.org>
2014-03-02 08:19:59 -0500

    * add comments about site_type

--- deploy/database/data.config.sql
+++ deploy/database/data.config.sql
@@ -1,6 +1,13 @@
 # Table data for configuration-related tables
 
+## Since config contains key/value pairs, add a comment for each
+## key indicating what types of values are expected
 DELETE FROM config;
 INSERT INTO config (conf_key, conf_value) VALUES
+
+# site_type should be one of: 'production', 'development'
+#
+# test-only or unstable features may be disabled on production,
+# and the UI may vary slightly to tell users which site they are on
 ('site_type', 'development');
 
