ba8fcc992af296f85d7e0bfcb481f967f4d84936
ManvilleFog <manvillefog@gmail.com>
2014-03-27 15:22:29 -0400

    fix Fightball to indicate button specials

--- deploy/database/data.button.sql
+++ deploy/database/data.button.sql
@@ -313,19 +313,19 @@ INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
 # FIGHTBALL (Cheapass Games) NOTE: special die selection rules - choose 5 dice out of all available (not implemented)
 # ASSUMED ALL TO BE TL
 INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
-('Brie',              '(4) (6) (8) (10) (12) (12) (20)',         0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Domino',            '(4) (4) (8) (8) (8) (10) (12)',           0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Echo(Fightball)',   '(4) (6) (6) (6) (12) (12) (12) (20)',     0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Georgia',           '(6) (6) (8) (8) (12) (20) (20)',          0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Gordo(Fightball)',  '(4) (6) (6) (8) (8) (10) (20)',           0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Mel',               '(4) (4) (8) (10) (10) (20) (20)',         0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Pavel',             '(6) (10) (10) (12) (12) (20) (20)',       0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Rocq',              '(6) (10) (10) (12) (20) (20) (20)',       0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Skinny',            '(4) (4) (4) (6) (8) (8) (10)',            0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Tex',               '(4) (4) (6) (8) (10) (10) (12) (12)',     0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Brie',              '(4) (6) (8) (10) (12) (12) (20)',         1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Domino',            '(4) (4) (8) (8) (8) (10) (12)',           1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Echo(Fightball)',   '(4) (6) (6) (6) (12) (12) (12) (20)',     1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Georgia',           '(6) (6) (8) (8) (12) (20) (20)',          1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Gordo(Fightball)',  '(4) (6) (6) (8) (8) (10) (20)',           1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Mel',               '(4) (4) (8) (10) (10) (20) (20)',         1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Pavel',             '(6) (10) (10) (12) (12) (20) (20)',       1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Rocq',              '(6) (10) (10) (12) (20) (20) (20)',       1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Skinny',            '(4) (4) (4) (6) (8) (8) (10)',            1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Tex',               '(4) (4) (6) (8) (10) (10) (12) (12)',     1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
 # Pepper and Zal are from the BM Trading Cards
-('Pepper',            '(6) (10) (10) (10) (12) (12) (12) (16)',  0, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
-('Zal',               '(4) (6) (6) (16) (16) (20) (20)',         0, 1, (SELECT id FROM buttonset WHERE name="Fightball"));
+('Pepper',            '(6) (10) (10) (10) (12) (12) (12) (16)',  1, 1, (SELECT id FROM buttonset WHERE name="Fightball")),
+('Zal',               '(4) (6) (6) (16) (16) (20) (20)',         1, 1, (SELECT id FROM buttonset WHERE name="Fightball"));
 
 # FREAKS (Cheapass Games)              INTRODUCES Queer(q) dice; Poison(p); Shadow(s); Speed(z)
 INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
