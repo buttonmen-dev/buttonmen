40368963bfbb0a009505cc7eda419aa32240490d
ManvilleFog <manvillefog@gmail.com>
2014-03-29 01:22:14 -0400

    Japanese Beetle (unofficial)

--- deploy/database/data.button.sql
+++ deploy/database/data.button.sql
@@ -736,6 +736,16 @@ INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
 ('Scarlet Jester',  'n(4) E(10) n(12) E(20) E(20)',               0, 0, (SELECT id FROM buttonset WHERE name="Gaming Guardians")),
 ('Shane Hensley',   'E(6) E(6) E(6) E(6) E(6) E(6)',              0, 0, (SELECT id FROM buttonset WHERE name="Gaming Guardians"));
 
+# JAPANESE BEETLE (unofficial)                 INTRODUCES Dodge(_)
+# unlicensed fan set designed by Bayani Caes long before official JB set was created
+INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
+('The Japanese Beetle(u)',  '(6) (12) (12) _(V) (X)',     0, 0, (SELECT id FROM buttonset WHERE name="Japanese Beetle (unofficial)")),
+('The Flying Squirrel(u)',  '(4) (6) (12) (20) (X)',      0, 0, (SELECT id FROM buttonset WHERE name="Japanese Beetle (unofficial)")),
+('Joe McCarthy',            '(10) (12) (12) (20) (X)',    0, 0, (SELECT id FROM buttonset WHERE name="Japanese Beetle (unofficial)")),
+('Kremlina',                '(6) (8) (10) (12) (X)',      0, 0, (SELECT id FROM buttonset WHERE name="Japanese Beetle (unofficial)")),
+('Max Factor',              '(6) (8) (12) (X) (X)',       0, 0, (SELECT id FROM buttonset WHERE name="Japanese Beetle (unofficial)")),
+('Frenchman',               '(8) (10) (10) (12) (X)',     0, 0, (SELECT id FROM buttonset WHERE name="Japanese Beetle (unofficial)"));
+
 
 
 ################################################
