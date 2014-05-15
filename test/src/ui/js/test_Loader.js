b1a8ed7c387ee2bfc52f5bbc7fd973e11a9e7fe5
AdmiralJota <jota@grunk.org>
2014-05-14 21:20:32 -0400

    Created Loader.js and made pages use it
    Converted all HTML pages on the site to use $.getScript() to load all
    their scripts (via Loader.js) in order to ensure they're not cached.

---
+++ test/src/ui/js/test_Loader.js
@@ -0,0 +1,36 @@
+module("Loader", {
+  'setup': function() {
+    // The usual default scripts would already have loaded with the testing
+    // harness, so we'll load a special dummy one instead
+    Loader.defaultScripts = [ 'js/dummy1.js', ];
+
+    BMTestUtils.LoaderPre = BMTestUtils.getAllElements();
+  },
+  'teardown': function() {
+
+    // Delete all elements we expect this module to create
+
+    // JS objects
+    delete Loader.callback;
+    delete Dummy;
+
+    // Fail if any other elements were added or removed
+    BMTestUtils.LoaderPost = BMTestUtils.getAllElements();
+    deepEqual(
+      BMTestUtils.LoaderPost, BMTestUtils.LoaderPre,
+      "After testing, the page should have no unexpected element changes");
+  }
+});
+
+// pre-flight test of whether the Loader module has been loaded
+test("test_Loader_is_loaded", function() {
+  ok(Verify, "The Loader namespace exists");
+});
+
+asyncTest("test_Loader.loadScripts", function() {
+  Loader.loadScripts([ 'js/dummy2.js' ], function () {
+    equal(Dummy.One, 1, "default script was loaded");
+    equal(Dummy.Two, 2, "explicit script was loaded");
+    start();
+  });
+});
