bd85ed038df3217bf87a62d375aa196e394e8d0e
James Ong <blackshadowshade@gmail.com>
2014-06-12 01:54:58 +1000

    Fixed UnusedFormalParameter warning

--- src/engine/BMBtnSkillGiant.php
+++ src/engine/BMBtnSkillGiant.php
@@ -3,7 +3,7 @@
 class BMBtnSkillGiant extends BMBtnSkill {
     public static $hooked_methods = array('is_button_slow');
 
-    public static function is_button_slow($args) {
+    public static function is_button_slow() {
         return array('is_button_slow' => TRUE);
     }
 }
