088c442ae54c7297ac41312614e6e2dbf8f66948
James Ong <james@assort.com>
2014-07-22 14:48:38 +1000

    Added description of Fire skill to BMSkillFire

--- src/engine/BMSkillFire.php
+++ src/engine/BMSkillFire.php
@@ -45,4 +45,19 @@ class BMSkillFire extends BMSkill {
 
         $args['possibleAssistValues'] = range(1, $die->value - $die->min);
     }
+
+    protected static function get_description() {
+        return 'Fire Dice cannot make Power Attacks. Instead, they can assist '.
+               'other Dice in making Skill and Power Attacks. Before making a '.
+               'Skill or Power Attack, you may increase the value showing on '.
+               'any of the attacking dice, and decrease the values showing on '.
+               'one or more of your Fire Dice by the same amount. For example, '.
+               'if you wish to increase the value of an attacking die by 5 points, '.
+               'you can take 5 points away from one or more of your Fire Dice. '.
+               'Turn the Fire Dice to show the adjusted values, and then make the '.
+               'attack as normal. Dice can never be increased or decreased outside '.
+               'their normal range, i.e., a 10-sided die can never show a number '.
+               'lower than 1 or higher than 10. Also, Fire Dice cannot assist other '.
+               'dice in making attacks other than normal Skill and Power Attacks.';
+    }
 }
