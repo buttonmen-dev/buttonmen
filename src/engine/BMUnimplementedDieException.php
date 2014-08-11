14198129c3667abb28ac66721b727ed9edd0ead2
AdmiralJota <jota@grunk.org>
2014-08-11 03:51:36 -0400

    #*$&@#

--- src/engine/BMUnimplementedDieException.php
+++ src/engine/BMUnimplementedDieException.php
@@ -4,4 +4,6 @@
  * Indicates that a die could not be loaded due to some feature of it not yet
  * being implemented.
  */
-class BMUnimplementedDieException extends Exception { }
+class BMUnimplementedDieException extends Exception {
+    
+}
