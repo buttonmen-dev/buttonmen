bdc341bf4632a11ee8aaab8c8b7695cc5b73387c
AdmiralJota <jota@grunk.org>
2014-08-16 01:47:28 -0400

    I hope you're happy now

--- deploy/database/views.button.sql
+++ deploy/database/views.button.sql
@@ -2,8 +2,9 @@
 
 DROP VIEW IF EXISTS button_view;
 CREATE VIEW button_view
-AS SELECT b.id, b.name, b.recipe, b.tourn_legal, b.btn_special,
-          b.flavor_text, s.name AS set_name, s.id AS set_id
+AS SELECT
+    b.id, b.name, b.recipe, b.tourn_legal, b.btn_special,
+    b.flavor_text, s.name AS set_name, s.sort_order AS set_sort_order
 FROM button AS b
     LEFT JOIN buttonset AS s ON b.set_id = s.id
 ORDER BY s.sort_order ASC, b.name ASC;
