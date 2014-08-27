c4a90d42640757ef77c19fb00df2151eeadf8a00
ManvilleFog <manvillefog@gmail.com>
2014-08-27 04:04:54 -0400

    removed changes to try and bring up to date

--- deploy/database/data.button.sql
+++ deploy/database/data.button.sql
@@ -55,7 +55,6 @@ INSERT INTO buttonset (name) VALUES
 ('Kubla Con'),
 ('Space Girlz'),
 ('Bridge and Tunnel'),
-('Big Top'),
 ('2005 Rare Promo'),
 ('High School Drama!'),
 ('Unexploded Cow'),
@@ -172,13 +171,6 @@ INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
 INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
 ('Bunnies',     '(1) (1) (1) (1) (X)',           0, 0, (SELECT id FROM buttonset WHERE name="The Big Cheese")),
 ('Lab Rat',     '(2) (2) (2) (2) (X)',           0, 0, (SELECT id FROM buttonset WHERE name="The Big Cheese"));
-
-#BIGTOP (APE games - Cassandra) 
-INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
-('Firebreather',       '(4) F(6) F(6) (12) (S)',          0, 0, (SELECT id FROM buttonset WHERE name="Big Top")),
-('Monkeys',            'z(6) z(6) z(6) z(10) z(T)',       0, 0, (SELECT id FROM buttonset WHERE name="Big Top")),
-('Ringmaster',         'f(6) f(6) f(8) f(8) (12)',        0, 0, (SELECT id FROM buttonset WHERE name="Big Top")),
-('Stumbling Clowns',   '(8) t(8) (10) t(10) (X)',         0, 0, (SELECT id FROM buttonset WHERE name="Big Top"));
 
 # BRAWL (Cheapass Games)              INTRODUCES Speed(z) dice
 INSERT INTO button (name, recipe, btn_special, tourn_legal, set_id) VALUES
