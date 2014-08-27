185df8ba1b2ed66f385265d36804086bfaffaa1c
James Ong <blackshadowshade@gmail.com>
2014-08-27 23:27:12 +1000

    Stop BMBtnSkillEcho->load_buttons hook from firing when the opponent's button has not yet been set

--- src/engine/BMBtnSkillEcho.php
+++ src/engine/BMBtnSkillEcho.php
@@ -11,11 +11,27 @@
 class BMBtnSkillEcho extends BMBtnSkill {
     public static $hooked_methods = array('load_buttons');
 
-    public static function load_buttons($args) {
+    public static function load_buttons(array $args) {
+        assert(array_key_exists('name', $args) &&
+                   array_key_exists('recipe', $args) &&
+                   array_key_exists('oppname', $args) &&
+                   array_key_exists('opprecipe', $args),
+               'load_buttons die hook is missing required input arguments');
+
+        assert(!empty($args['name']), 'Button name may not be empty.');
+
+        // don't do any recipe copying until the opponent button exists
+        if (empty($args['oppname'])) {
+            return;
+        }
+
         $newRecipe = $args['recipe'];
 
+        // copy opponent's recipe only if Echo hasn't yet got a recipe
         if ('' == $args['recipe']) {
             if ('' == $args['opprecipe']) {
+                // opponent's button recipe is empty, which only occurs with Echo and Zero
+                // thus choose a default recipe
                 $newRecipe = '(4) (4) (10) (12) (X)';
             } else {
                 $newRecipe = $args['opprecipe'];
