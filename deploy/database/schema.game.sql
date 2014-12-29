add5783d0a918d8095f233060d4d2714c049f102
James Ong <blackshadowshade@gmail.com>
2014-12-29 23:02:53 +1100

    Added DHS button vs button stats table

--- deploy/database/schema.game.sql
+++ deploy/database/schema.game.sql
@@ -137,3 +137,12 @@ CREATE TABLE tournament (
     creator_id         SMALLINT UNSIGNED NOT NULL,
     description        VARCHAR(255) NOT NULL
 );
+
+DROP TABLE IF EXISTS dhs_site_button_vs_button_stats;
+CREATE TABLE dhs_site_button_vs_button_stats (
+    dhs_button_id_a     SMALLINT UNSIGNED NOT NULL,
+    dhs_button_id_b     SMALLINT UNSIGNED NOT NULL,
+    games_button_a_won  MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+    games_button_b_won  MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+    date_compiled       TIMESTAMP
+);
--- deploy/database/updates/00614_stats_tables_01.sql
+++ deploy/database/updates/00614_stats_tables_01.sql
@@ -1,1 +1,10 @@
 ALTER TABLE button ADD dhs_id SMALLINT UNSIGNED;
+
+DROP TABLE IF EXISTS dhs_site_button_vs_button_stats;
+CREATE TABLE dhs_site_button_vs_button_stats (
+    dhs_button_id_a     SMALLINT UNSIGNED NOT NULL,
+    dhs_button_id_b     SMALLINT UNSIGNED NOT NULL,
+    games_button_a_won  MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+    games_button_b_won  MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+    date_compiled       TIMESTAMP
+);
