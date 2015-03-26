23df297f069f888e1ff50f69caea0234614c7134
James Ong <blackshadowshade@gmail.com>
2015-03-27 01:10:37 +1100

    Removed debugging comment

--- test/src/api/responderTest.php
+++ test/src/api/responderTest.php
@@ -8920,7 +8920,6 @@ class responderTest extends PHPUnit_Framework_TestCase {
     }
 
     /**
-     * @group fulltest_deps
      * @depends test_request_savePlayerInfo
      *
      * This game tests RandomBMMixed and verifies that trip attacks fail against too-large shadow maximum dice
