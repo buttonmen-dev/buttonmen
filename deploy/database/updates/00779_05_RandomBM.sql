3c424c7b833e4a7bdc55ebf05e0bb0312334cc43
James Ong <blackshadowshade@gmail.com>
2015-04-02 00:11:53 +1100

    Fixed update file

--- deploy/database/updates/00779_05_RandomBM.sql
+++ deploy/database/updates/00779_05_RandomBM.sql
@@ -1,15 +1,17 @@
-DELETE FROM button
-WHERE set_id = (SELECT id FROM buttonset WHERE name="RandomBM");
+START TRANSACTION;
+
+UPDATE game_player_map SET button_id = 20000 WHERE button_id=(SELECT id FROM button WHERE name = "RandomBMFixed");
+UPDATE game_player_map SET button_id = 20001 WHERE button_id=(SELECT id FROM button WHERE name = "RandomBMAnime");
+
+DELETE FROM button WHERE name="RandomBMFixed";
+DELETE FROM button WHERE name="RandomBMAnime";
 
 INSERT INTO button (id, name, recipe, btn_special, tourn_legal, set_id) VALUES
-(10001, 'RandomBMVanilla', '', 1, 0, (SELECT id FROM buttonset WHERE name="RandomBM")),
-(10002, 'RandomBMAnime',   '', 1, 0, (SELECT id FROM buttonset WHERE name="RandomBM")),
-(10003, 'RandomBMMixed',   '', 1, 0, (SELECT id FROM buttonset WHERE name="RandomBM")),
-(10004, 'RandomBMFixed',   '', 1, 0, (SELECT id FROM buttonset WHERE name="RandomBM"));
+(10002, 'RandomBMAnime', '', 1, 0, (SELECT id FROM buttonset WHERE name="RandomBM")),
+(10004, 'RandomBMFixed', '', 1, 0, (SELECT id FROM buttonset WHERE name="RandomBM"));
 
-UPDATE button
-SET flavor_text='This button gets a different random recipe in each game, with a vanilla random formula: 5 dice, no swing dice, no skills'
-WHERE name='RandomBMVanilla';
+UPDATE game_player_map SET button_id = (SELECT id FROM button WHERE name = "RandomBMAnime") WHERE button_id=20000;
+UPDATE game_player_map SET button_id = (SELECT id FROM button WHERE name = "RandomBMFixed") WHERE button_id=20001;
 
 UPDATE button
 SET flavor_text='This button gets a different random recipe in each game, with a fixed random formula: 5 dice, no swing dice, two of them having a single skill chosen from c, f, and d (the same skill on both)'
@@ -22,3 +24,5 @@ WHERE name='RandomBMMixed';
 UPDATE button
 SET flavor_text='This button gets a different random recipe in each game, with a fixed anime formula: 4 normal dice and 4 reserve dice, chosen from standard die sizes'
 WHERE name='RandomBMAnime';
+
+COMMIT;
