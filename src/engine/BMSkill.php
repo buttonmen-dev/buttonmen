b0ae42e1d74742de28cb6a942148fa9208d64c76
James Ong <blackshadowshade@gmail.com>
2015-04-08 22:50:32 +1000

    Removed PHPDoc warnings for BMSkill class

--- src/engine/BMSkill.php
+++ src/engine/BMSkill.php
@@ -135,6 +135,11 @@ class BMSkill {
                      'Weak'         => 'h');
     }
 
+    /**
+     * All possible skill characters
+     *
+     * @return array
+     */
     public static function all_skill_chars() {
         return array_values(self::skill_name_abbreviation_mapping());
     }
