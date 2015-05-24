a10fa3c6a21da5b369d015b3a9f58af119a5bf0b
James Ong <blackshadowshade@gmail.com>
2015-05-25 02:31:31 +1000

    Fixed Python tests

--- test/tools/api-client/python/lib/test_bmapi.py
+++ test/tools/api-client/python/lib/test_bmapi.py
@@ -78,7 +78,7 @@ class TestBMClient(unittest.TestCase):
     r = self.obj.load_active_games()
     self.assertEqual(r.status, 'ok', 'loadActiveGames returns successfully')
     known_keys = [
-      'gameIdArray', 'gameStateArray', 'inactivityArray',
+      'gameIdArray', 'gameStateArray', 'inactivityArray', 'inactivityRawArray',
       'isAwaitingActionArray', 'myButtonNameArray', 'nDrawsArray',
       'nLossesArray', 'nTargetWinsArray', 'nWinsArray',
       'opponentButtonNameArray', 'opponentColorArray', 'opponentIdArray',
@@ -95,7 +95,7 @@ class TestBMClient(unittest.TestCase):
     r = self.obj.load_completed_games()
     self.assertEqual(r.status, 'ok', 'loadCompletedGames returns successfully')
     known_keys = [
-      'gameIdArray', 'gameStateArray', 'inactivityArray',
+      'gameIdArray', 'gameStateArray', 'inactivityArray', 'inactivityRawArray',
       'isAwaitingActionArray', 'myButtonNameArray', 'nDrawsArray',
       'nLossesArray', 'nTargetWinsArray', 'nWinsArray',
       'opponentButtonNameArray', 'opponentColorArray', 'opponentIdArray',
