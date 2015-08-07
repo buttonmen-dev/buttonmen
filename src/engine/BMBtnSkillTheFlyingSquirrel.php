f49c03f527f44c167a25a836b61df6ec3a8d3ed1
James Ong <blackshadowshade@gmail.com>
2015-08-08 02:56:30 +1000

    Added button-level skill for TheFlyingSquirrel

---
+++ src/engine/BMBtnSkillTheFlyingSquirrel.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * BMBtnSkillTheFlyingSquirrel: Code specific to The Flying Squirrel
+ *
+ * @author: james
+ */
+
+/**
+ * This class currently supports the special skills of The Flying Squirrel
+ */
+class BMBtnSkillTheFlyingSquirrel extends BMBtnSkill {
+    /**
+     * An array containing the names of functions run by
+     * BMCanHaveSkill->run_hooks()
+     *
+     * @var array
+     */
+    public static $hooked_methods = array('attack_list');
+
+    /**
+     * Hooked method applied when determining possible attack types
+     *
+     * @param array $args
+     */
+    public static function attack_list($args) {
+        if (!is_array($args)) {
+            return;
+        }
+
+        $attackTypeArray = &$args['attackTypeArray'];
+
+        foreach (self::incompatible_attack_types() as $attackType) {
+            if (array_key_exists($attackType, $attackTypeArray)) {
+                unset($attackTypeArray[$attackType]);
+            }
+        }
+    }
+
+    /**
+     * Attack types incompatible with this skill type
+     *
+     * @return array
+     */
+    public static function incompatible_attack_types() {
+        return array('Skill');
+    }
+}
