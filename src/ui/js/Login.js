c5cf8510447026bbbca4d7753991b621a205c425
James Ong <blackshadowshade@gmail.com>
2015-09-09 02:40:32 +1000

    Fixed code to use new c_body and c_footer divs

--- src/ui/js/Login.js
+++ src/ui/js/Login.js
@@ -198,7 +198,7 @@ Login.arrangeBody = function(page, form, submitSelector) {
   }
 
   if ($('#' + Login.pageModule.bodyDivId).length === 0) {
-    $('body').append($('<div>', {
+    $('#c_body').append($('<div>', {
       'id': Login.pageModule.bodyDivId,
       'class': 'mainBody',
     }));
@@ -213,10 +213,9 @@ Login.arrangeBody = function(page, form, submitSelector) {
 };
 
 Login.arrangeFooter = function() {
-  if ($('#footer').length === 0) {
-    $('c_footer').append($('<hr>', { 'id': 'footer_separator', }));
-    $('c_footer').append($('<div>', {'id': 'footer', }));
-  }
+  $('#c_footer').append($('<hr>', { 'id': 'footer_separator', }));
+  $('#c_footer').append($('<div>', {'id': 'footer', }));
+
   $('#footer').empty();
   $('#footer').append(Login.footer);
 };
