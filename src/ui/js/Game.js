3e4421c5f4d64e83e3609f0604863e97fc1c76ac
James Ong <blackshadowshade@gmail.com>
2015-12-06 14:57:42 +1100

    Changed 'cancel' to 'withdraw'

--- src/ui/js/Game.js
+++ src/ui/js/Game.js
@@ -368,7 +368,7 @@ Game.actionShowRejectedGame = function() {
   Game.form = null;
 
   Game.page = $('<div>');
-  Game.pageAddGameHeader('This game has been rejected or cancelled');
+  Game.pageAddGameHeader('This game has been cancelled');
 
   var dieEndgameTable = $('<table>');
   var dieEndgameTr = $('<tr>');
@@ -1556,7 +1556,7 @@ Game.formCancelGame = function(e) {
   e.preventDefault();
 
   var doGameCancel = Env.window.confirm(
-    'Are you SURE you want to cancel this game?'
+    'Are you SURE you want to withdraw this game?'
   );
   if (!doGameCancel) {
     return;
@@ -1572,7 +1572,7 @@ Game.formCancelGame = function(e) {
     'gameId': $(this).attr('data-gameId'),
   };
   var messages = {
-    'ok': { 'type': 'fixed', 'text': 'Successfully cancelled game', },
+    'ok': { 'type': 'fixed', 'text': 'Successfully withdrew game', },
     'notok': { 'type': 'server' },
   };
   Api.apiFormPost(
@@ -1790,7 +1790,7 @@ Game.pageAddGameHeader = function(action_desc) {
       acceptRejectDiv.append(rejectLink);
     } else {
       var cancelLink = $('<a>', {
-        'text': '[Cancel Game]',
+        'text': '[Withdraw Game]',
         'href': '#',
         'data-gameId': Api.game.gameId,
       });
