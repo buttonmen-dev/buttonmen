2f28efa4bef5759392a46fe3457c87c94911e1ba
Josh Smift <irilyth@infersys.com>
2015-12-06 13:02:33 -0500

    Print the JSON dump, so it's prettier.

--- tools/api-client/python/interactive_mode.py
+++ tools/api-client/python/interactive_mode.py
@@ -1,5 +1,6 @@
-# Here's an example of stuff to copy and paste into an interactive Python interpreter to get a connection loaded.
-# Or you can load it with 'python -i interactive-mode.py'.
+# Here's an example of stuff to copy and paste into an interactive Python
+# interpreter to get a connection loaded.
+# Or you can load it with 'python -i interactive_mode.py'.
 
 # Set some variables.
 
@@ -18,9 +19,10 @@ bmconnection = bmutils.BMClientParser(os.path.expanduser(bmrc), site)
 if not bmconnection.verify_login():
   print "Could not login"
     
-# At this point you can do whatever you want. Here's how to load a game, and print its info in nice JSON.
+# At this point you can do whatever you want. Here's how to load a game,
+# and print its info in nice JSON.
 
 gamenumber = 3038
 
 game = bmconnection.wrap_load_game_data(gamenumber)
-json.dump(game, sys.stdout, indent=1, sort_keys=True)
+print json.dumps(game, sys.stdout, indent=1, sort_keys=True)
