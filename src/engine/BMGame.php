ead372b4bbdab38ca90b8491fc77f6f2ba1cda20
James Ong <blackshadowshade@gmail.com>
2016-02-09 00:10:12 +1100

    Cause degenerate games to move to the CANCELLED game state after 200 rounds

--- src/engine/BMGame.php
+++ src/engine/BMGame.php
@@ -1832,6 +1832,12 @@ class BMGame {
      * Perform the logic required at BMGameState::end_round
      */
     protected function do_next_step_end_round() {
+        // stop degenerate games from running forever
+        if ($this->roundNumber >= 200) {
+            $this->gameState = BMGameState::CANCELLED;
+            return;
+        }
+
         $roundScoreArray = array();
         foreach ($this->playerArray as $player) {
             $roundScoreArray[] = $player->roundScore;
