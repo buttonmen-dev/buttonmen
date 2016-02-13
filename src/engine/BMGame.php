fc0586a966b4c867e31e69beeb7526f4d78fe340
James Ong <blackshadowshade@gmail.com>
2016-02-14 02:25:31 +1100

    Update prevOptValueArray when adding a reserve die

--- src/engine/BMGame.php
+++ src/engine/BMGame.php
@@ -715,6 +715,7 @@ class BMGame {
     protected function update_game_state_choose_reserve_dice() {
         // if all decisions on reserve dice have been made
         if (!$this->isWaitingOnAnyAction()) {
+            $this->update_prevOptValueArray();
             $areAnyDiceAdded = $this->add_selected_reserve_dice();
 
             if (array_sum($areAnyDiceAdded) > 0) {
@@ -724,12 +725,43 @@ class BMGame {
                         $player->button->update_button_recipe();
                     }
                 }
-
             }
 
             $this->update_opt_requests_to_ignore_reserve_dice();
             $this->remove_dice_with_skill('Reserve');
             $this->gameState = BMGameState::SPECIFY_DICE;
+        }
+    }
+
+    /**
+     * Update option request array when reserve option dice are added
+     */
+    protected function update_prevOptValueArray() {
+        foreach ($this->playerArray as $player) {
+            if (!empty($player->prevOptValueArray)) {
+                foreach (array_reverse(array_keys($player->activeDieArray)) as $dieIdx) {
+                    if ($player->activeDieArray[$dieIdx]->has_flag('AddReserve')) {
+                        // count how many dice don't have reserve on the left of the added die
+                        $dieIndicesWithoutReserve = $player->die_indices_without_reserve();
+                        $nDiceOnLeft = 0;
+                        foreach ($dieIndicesWithoutReserve as $leftDieIdx) {
+                            if ($leftDieIdx < $dieIdx) {
+                                $nDiceOnLeft++;
+                            }
+                        }
+
+                        // this code theoretically accommodates the ability to add multiple reserve
+                        // option dice at once
+                        foreach (array_reverse(array_keys($player->prevOptValueArray)) as $optIdx) {
+                            // increment all keys for prevOptValueArray that aren't left of the new die
+                            if ($optIdx >= $nDiceOnLeft) {
+                                $player->prevOptValueArray[$optIdx + 1] = $player->prevOptValueArray[$optIdx];
+                                unset($player->prevOptValueArray[$optIdx]);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
