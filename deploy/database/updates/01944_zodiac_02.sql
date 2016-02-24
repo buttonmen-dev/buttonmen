3baaf8d9ba28cd46ccc7977579e0b2d22e913b1a
James Ong <james@assort.com>
2016-02-24 11:12:58 +1100

    Changed Zodiac recipes

---
+++ deploy/database/updates/01944_zodiac_02.sql
@@ -0,0 +1,15 @@
+UPDATE button
+SET recipe = '(4) s(6) (10) g(12) (X)'
+WHERE name = 'Capricorn';
+
+UPDATE button
+SET recipe = '(4,4) (12) (12) z(X) s(X)'
+WHERE name = 'Gemini';
+
+UPDATE button
+SET recipe = '(4) (4) s(8) p(10) (V)'
+WHERE name = 'Sagittarius';
+
+UPDATE button
+SET recipe = 'g(8) (10) s(12) (20) (V)'
+WHERE name = 'Taurus';
