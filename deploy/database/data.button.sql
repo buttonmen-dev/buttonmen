56daec01a179ccadad9e4bd7430444e53c05b09e
James Ong <james@assort.com>
2016-02-29 09:26:02 +1100

    Fixed recipe of Libra

--- deploy/database/data.button.sql
+++ deploy/database/data.button.sql
@@ -939,7 +939,7 @@ INSERT INTO button (id, name, recipe, btn_special, tourn_legal, set_id) VALUES
 (698, 'Capricorn',   '(4) s(6) (10) g(12) (X)',             0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
 (699, 'Gemini',      '(4,4) (12) (12) z(X) s(X)',           0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
 (700, 'Leo',         '(4) (6) p(10) z(20) (X)',             0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
-(701, 'Libra',       '(4) g(4) (8,8) z(8,8) (V)',           0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
+(701, 'Libra',       '(4) g(4) z(8,8) (8,8) (V)',           0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
 (702, 'Pisces',      '(4) p(8) z(12) (12) (X)',             0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
 (703, 'Sagittarius', '(4) (4) s(8) p(10) (V)',              0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
 (704, 'Scorpio',     'g(6) (10) (12) p(20) (V)',            0, 0, (SELECT id FROM buttonset WHERE name="Zodiac")),
---
+++ deploy/database/updates/01944_zodiac_03.sql
@@ -0,0 +1,3 @@
+UPDATE button
+SET recipe = '(4) g(4) z(8,8) (8,8) (V)'
+WHERE name = 'Libra';
