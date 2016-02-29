cf6361c13f5acc3fbf76dee2ded1117b5c1081d0
James Ong <james@assort.com>
2016-02-29 16:50:19 +1100

    Fixed single quotes in flavour texts for Aries and Capricorn

--- deploy/database/updates/01944_zodiac_03.sql
+++ deploy/database/updates/01944_zodiac_03.sql
@@ -1,3 +1,6 @@
 UPDATE button
 SET recipe = '(4) g(4) z(8,8) (8,8) (V)'
 WHERE name = 'Libra';
+
+UPDATE button SET flavor_text="Aries is a strong-willed warrior and a bundle of energy. He is creative and spontaneous, quick to anger, but doesn’t take it personally. Although he works best alone, he likes parties, humor, and beating people up." WHERE name="Aries";
+UPDATE button SET flavor_text="Capricorn is a good organizer with broad shoulders for other people's problems. He is highly intelligent and shrugs off any negative comment about his personality. He loves stability and order, and beating people up." WHERE name="Capricorn";
