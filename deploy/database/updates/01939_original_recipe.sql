bd7c66b9b075dda706df5edf62d0bccc9ecee0b6
James Ong <blackshadowshade@gmail.com>
2016-03-01 01:28:06 +1100

    Added SQL to back-fill original_recipe for already-existing games

--- deploy/database/updates/01939_original_recipe.sql
+++ deploy/database/updates/01939_original_recipe.sql
@@ -34,3 +34,28 @@ LEFT JOIN button AS b
 ON m.button_id = b.id
 LEFT JOIN game AS g
 ON m.game_id = g.id;
+
+-- first copy the canonical recipes directly from the button table
+UPDATE game_player_map
+SET original_recipe = (SELECT recipe
+  FROM button
+  WHERE button.id = game_player_map.button_id
+)
+WHERE original_recipe IS NULL;
+
+-- for Echo and Zero, explicitly copy the opponent's original recipe
+UPDATE game_player_map AS m1
+SET m1.original_recipe = (SELECT m2.original_recipe
+  FROM (select game_id, player_id, original_recipe FROM game_player_map) AS m2
+  WHERE m1.game_id = m2.game_id
+  AND m1.player_id <> m2.player_id
+)
+WHERE button_id IN (SELECT b.id
+  FROM button AS b
+  WHERE b.name IN ('Echo', 'Zero')
+);
+
+-- now deal with recipes that remain empty by directly copying the recipes
+UPDATE game_player_map
+SET original_recipe = alt_recipe
+WHERE original_recipe = '';
