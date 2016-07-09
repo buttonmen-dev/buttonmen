ce7c1dc55eafbb98ac651d8427ee82c4c09000b0
Chaos Golubitsky <walrus-github@glassonion.org>
2016-07-09 15:15:54 -0400

    Other half of migration of choose_die_values action log entries to new format

--- deploy/database/schema.game.sql
+++ deploy/database/schema.game.sql
@@ -135,6 +135,29 @@ CREATE TABLE game_action_log_type_play_another_turn (
     INDEX (action_log_id)
 );
 
+CREATE TABLE game_action_log_type_choose_die_values (
+    id                 INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    action_log_id      INTEGER UNSIGNED NOT NULL,
+    round_number       TINYINT UNSIGNED NOT NULL,
+    INDEX (action_log_id)
+);
+
+CREATE TABLE game_action_log_type_choose_die_values_swing (
+    id                 INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    action_log_id      INTEGER UNSIGNED NOT NULL,
+    swing_type         CHAR NOT NULL,
+    swing_value        TINYINT UNSIGNED,
+    INDEX (action_log_id)
+);
+
+CREATE TABLE game_action_log_type_choose_die_values_option (
+    id                 INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    action_log_id      INTEGER UNSIGNED NOT NULL,
+    recipe             VARCHAR(20) NOT NULL,
+    option_value       TINYINT UNSIGNED,
+    INDEX (action_log_id)
+);
+
 CREATE TABLE game_chat_log (
     id                 INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     game_id            MEDIUMINT UNSIGNED NOT NULL,
