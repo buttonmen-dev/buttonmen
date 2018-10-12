96efcc148514acbe0ba960b8b746a3b9be4a7fea
James Ong <blackshadowshade@gmail.com>
2018-10-13 00:08:58 +1100

    Updated recipes again

---
+++ deploy/database/updates/02360_peloton_03.sql
@@ -0,0 +1,5 @@
+UPDATE button SET recipe = 'c(4) M(8) ^(10) k(10) (X)' WHERE name = 'Doyle';
+UPDATE button SET recipe = 'H(4) %(8) f(10) d(10) (X)' WHERE name = 'Julia';
+UPDATE button SET recipe = 'M(4) k(8) G(10) H(10) (X)' WHERE name = 'Mariusz';
+UPDATE button SET recipe = 'k(4) s(8) g(10) M(10) (X)' WHERE name = 'Orlando';
+UPDATE button SET recipe = 'd(4) s(8) z(10) h(10) (X)' WHERE name = 'Timea';
