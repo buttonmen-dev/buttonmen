24a990eedd00dbef27915d7148ca319c192fb2d5
Chaos Golubitsky <walrus-github@glassonion.org>
2018-10-17 14:58:39 -0400

    Install rsync and wget

--- deploy/vagrant/modules/apt/manifests/init.pp
+++ deploy/vagrant/modules/apt/manifests/init.pp
@@ -3,4 +3,11 @@ class apt::client {
     "apt_client_update":
       command => "/usr/bin/apt-get update";
   }
+
+  # Make sure generic base packages are installed, for the benefit
+  # of minimal environments like CircleCI docker
+  package {
+    "rsync": ensure => installed;
+    "wget": ensure => installed;
+  }
 }
