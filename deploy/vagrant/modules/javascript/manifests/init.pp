3996020a0cc830588af72fccc104961afadfd402
Chaos Golubitsky <walrus-github@glassonion.org>
2018-10-24 17:57:24 -0400

    Install phantomjs via wget of an older version, rather than via apt, to work around https://github.com/ariya/phantomjs/issues/14376

--- deploy/vagrant/modules/javascript/manifests/init.pp
+++ deploy/vagrant/modules/javascript/manifests/init.pp
@@ -1,7 +1,5 @@
 class javascript::type::circleci {
   package {
-    "phantomjs": ensure => installed;
-#    "jscoverage": ensure => installed;
     "npm": ensure => installed;
     "nodejs-legacy": ensure => installed;
   }
@@ -10,5 +8,17 @@ class javascript::type::circleci {
     "/usr/local/etc/run-jscover-qunit.js":
       ensure => file,
       content => template("javascript/run-jscover-qunit.js.erb");
+
+    "/usr/local/bin/install_phantomjs"
+      ensure => file,
+      mode => 544,
+      content => template("javascript/install_phantomjs.erb");
+  }
+
+  exec {
+    "javascript_install_phantomjs":
+      command => "/usr/local/bin/install_phantomjs",
+      creates => "/usr/local/bin/phantomjs",
+      require => File["/usr/local/bin/install_phantomjs"];
   }
 }
