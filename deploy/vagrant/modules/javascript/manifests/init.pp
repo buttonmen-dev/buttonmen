57149b437204101e730709955663ecd84e898bf3
Chaos Golubitsky <walrus-github@glassonion.org>
2018-10-24 19:08:51 -0400

    more debugging

--- deploy/vagrant/modules/javascript/manifests/init.pp
+++ deploy/vagrant/modules/javascript/manifests/init.pp
@@ -2,6 +2,9 @@ class javascript::type::circleci {
   package {
     "npm": ensure => installed;
     "nodejs-legacy": ensure => installed;
+
+    # we'll downgrade phantomjs later, but install it via apt to get its dependencies
+    "phantomjs": ensure => installed;
   }
 
   file {
@@ -14,11 +17,5 @@ class javascript::type::circleci {
     "javascript_wget_phantomjs":
       command => "/usr/bin/wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2 -O /usr/local/src/phantomjs-1.9.7-linux-x86_64.tar.bz2",
       creates => "/usr/local/src/phantomjs-1.9.7-linux-x86_64.tar.bz2";
-
-    "javascript_install_phantomjs":
-      command => "/bin/tar xjf /usr/local/src/phantomjs-1.9.7-linux-x86_64.tar.bz2 --strip-components=1",
-      cwd => "/usr/local",
-      creates => "/usr/local/bin/phantomjs",
-      require => Exec["javascript_wget_phantomjs"];
   }
 }
