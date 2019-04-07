b215414d1149b89ff0692f11e4a41d52e012442a
James Ong <blackshadowshade@gmail.com>
2019-04-07 21:19:24 +1000

    Added flavour text for Vysion and the Geekz button set

---
+++ deploy/database/updates/02446_flavour_01.sql
@@ -0,0 +1,10 @@
+# Geekz
+
+UPDATE button SET flavor_text="Caine struggles with a soul that is salved by darkness but addicted to the heat of the limelight. He composes poetry in engrish and delights in the ironic cruelty of beating people up." WHERE name="Caine";
+UPDATE button SET flavor_text="Xylene is colorless and sweet-smelling. When subjected to fanboys, she will exercise her right to beat people up." WHERE name="Xylene";
+UPDATE button SET flavor_text="Cammy Neko is a cosplayer who likes gashapon, nuigurumi, yaoi, and many other Japanese words. She often cosplays with friends as fighting game characters, who are always looking to beat people up." WHERE name="Cammy Neko";
+UPDATE button SET flavor_text="Sailor Man likes eating, shopping, and playing video games. He also likes crossplaying while beating people up." WHERE name="Sailor Man";
+
+# Vysion
+
+UPDATE button SET flavor_text="Perpetually winking, this druid-monk-ranger reads, solves puzzles, plays video games, and consumes massive amounts of popular culture yet considerately makes time to beat people up." WHERE name="Vysion";
