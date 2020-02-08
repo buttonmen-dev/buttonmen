d81629e1126453135490d44bb5f8250de71208d4
James Ong <blackshadowshade@gmail.com>
2020-02-08 17:22:52 +1100

    Added FAQ text

--- test/src/ui/js/test_Faq.js
+++ test/src/ui/js/test_Faq.js
@@ -48,8 +48,8 @@ test("test_Faq.showLoggedInPage", function(assert) {
   BMTestUtils.setupFakeLogin();
 
   Faq.showLoggedInPage();
-  var item = document.getElementById('faq_page');
-  assert.equal(item.nodeName, "DIV",
+  var node = document.getElementById('faq_page');
+  assert.equal(node.nodeName, "DIV",
         "#faq_page is a div after showLoggedInPage() is called");
 
   BMTestUtils.cleanupFakeLogin();
@@ -57,24 +57,129 @@ test("test_Faq.showLoggedInPage", function(assert) {
 
 test("test_Faq.showLoggedOutPage", function(assert) {
   Faq.showLoggedOutPage();
-  var item = document.getElementById('faq_page');
-  assert.equal(item.nodeName, "DIV",
+  var node = document.getElementById('faq_page');
+  assert.equal(node.nodeName, "DIV",
         "#faq_page is a div after showLoggedOutPage() is called");
 });
 
 test("test_Faq.showPage", function(assert) {
   Faq.showPage();
-  var item = document.getElementById('faq_page');
-  assert.equal(item.nodeName, "DIV",
+  var node = document.getElementById('faq_page');
+  assert.equal(node.nodeName, "DIV",
         "#faq_page is a div after showPage() is called");
 });
 
 test("test_Faq.bodyText", function(assert) {
-  var text = Faq.bodyText();
-  assert.equal(text[0].nodeName, "DIV", "bodyText returns a div");
+  var node = Faq.bodyText();
+  assert.equal(node[0].nodeName, "DIV", "bodyText returns a div");
 });
 
 test("test_Faq.generalInfo", function(assert) {
-  var text = Faq.generalInfo();
-  assert.equal(text[0].nodeName, "DIV", "generalInfo returns a div");
+  var node = Faq.generalInfo();
+  assert.equal(node[0].nodeName, "DIV", "generalInfo returns a div");
+});
+
+test("test_Faq.tableOfContents", function(assert) {
+  var node = Faq.generalInfo();
+  assert.equal(node[0].nodeName, "DIV", "tableOfContents returns a DIV");
+  assert.equal(node[0].childNodes[0].nodeName, "H1", "toc start with an H1");
+  assert.equal(node[0].childNodes[1].nodeName, "UL", "toc contain a UL");
+});
+
+test("test_Faq.gameplayLinks", function(assert) {
+  var node = Faq.gameplayLinks();
+  assert.equal(node[0].nodeName, "LI", "gameplayLinks returns a LI");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "links start with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "UL", "links contain a UL");
+});
+
+
+test("test_Faq.userprefsLinks", function(assert) {
+  var node = Faq.userprefsLinks();
+  assert.equal(node[0].nodeName, "LI", "userprefsLinks returns a LI");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "links start with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "UL", "links contain a UL");
+});
+
+test("test_Faq.unansweredLink", function(assert) {
+  var node = Faq.unansweredLink();
+  assert.equal(node[0].nodeName, "LI", "link is a LI");
+});
+
+test("test_Faq.content", function(assert) {
+  var node = Faq.content();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+});
+
+test("test_Faq.gameplayContent", function(assert) {
+  var node = Faq.gameplayContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H2", "content contains a H2");
+});
+
+test("test_Faq.defaultAttackContent", function(assert) {
+  var node = Faq.defaultAttackContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H3", "content contains a H3");
+});
+
+test("test_Faq.buttonSpecialContent", function(assert) {
+  var node = Faq.buttonSpecialContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H3", "content contains a H3");
+});
+
+test("test_Faq.flyingSquirrelContent", function(assert) {
+  var node = Faq.flyingSquirrelContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H3", "content contains a H3");
+});
+
+test("test_Faq.japaneseBeetleContent", function(assert) {
+  var node = Faq.japaneseBeetleContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H3", "content contains a H3");
+});
+
+test("test_Faq.userprefsContent", function(assert) {
+  var node = Faq.userprefsContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H2", "content contains a H2");
+});
+
+test("test_Faq.monitorContent", function(assert) {
+  var node = Faq.monitorContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H3", "content contains a H3");
+});
+
+test("test_Faq.fireOvershootingContent", function(assert) {
+  var node = Faq.fireOvershootingContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H3", "content contains a H3");
+});
+
+test("test_Faq.unansweredContent", function(assert) {
+  var node = Faq.unansweredContent();
+  assert.equal(node[0].nodeName, "#document-fragment",
+    "content contains a document fragment");
+  assert.equal(node[0].childNodes[0].nodeName, "A", "content starts with an A");
+  assert.equal(node[0].childNodes[1].nodeName, "H3", "content contains a H3");
 });
