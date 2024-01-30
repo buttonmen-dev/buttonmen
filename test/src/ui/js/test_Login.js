95d4fcd94adba7658db5a9e315ea95ca5fb95c9d
James Ong <james@assort.com>
2024-01-30 21:54:29 +1100

    Yearly update of copyright year to 2024

--- test/src/ui/js/test_Login.js
+++ test/src/ui/js/test_Login.js
@@ -87,7 +87,7 @@ test("test_Login.getFooter", function(assert) {
         [ "BR" ]
     ] ],
     [ "DIV", {}, [
-      "Button Men is copyright 1999, 2023 James Ernest and Cheapass Games: ",
+      "Button Men is copyright 1999, 2024 James Ernest and Cheapass Games: ",
       [ "A", { "href": "http://cheapass.com" }, [ "cheapass.com" ] ],
       " and ",
       [ "A", { "href": "http://beatpeopleup.cheapass.com" }, [ "beatpeopleup.cheapass.com" ] ],
